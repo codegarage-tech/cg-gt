a271639b5d9753ae63a026025c53dbe97556e718
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2018-06-29 09:20:38 +0600

    cg-gt: added some libraries
    - Arduino JSON library
    - Arduino SIM800L HTTP library
    - Arduino TinyGPSPlus library

--- cg-gt.ino
+++ cg-gt.ino
@@ -1,4 +1,10 @@
-#include "/home/rashed/BattleField/Weapon/development/Arduino/cg-gt/TinyGPS/TinyGPS.h"
+//Imports from linux
+#include "/home/rashed/BattleField/Weapon/development/Arduino/GPS Tracker/cg-gt/TinyGPSPlus/src/TinyGPS++.h"
+#include "/home/rashed/BattleField/Weapon/development/Arduino/GPS Tracker/cg-gt/ArduinoSIM800L/Http.h"
+#include "/home/rashed/BattleField/Weapon/development/Arduino/GPS Tracker/cg-gt/ArduinoJson/ArduinoJson.h"
+#include "SoftwareSerial.h"
+
+//Imports from windows
 
 void setup() {
   // put your setup code here, to run once:
